79d79282fcf9c3c0ff00fec64e19584aa7afdfdc
Ozum <ozum@ozum.net>
2018-05-10 17:18:36 +0300

    fix: update commitizen script

--- src/helper-scripts/commitizen.js
+++ src/helper-scripts/commitizen.js
@@ -3,9 +3,10 @@
 // Commitizen for multi-repo projects. See: https://github.com/commitizen/cz-cli
 const path = require("path");
 const { bootstrap } = require("commitizen/dist/cli/git-cz");
+const project = require("../project");
 
 bootstrap({
-  cliPath: path.join(__dirname, "../../../../commitizen"),
+  cliPath: project.fromRoot("node_modules/commitizen"), // path.join(__dirname, "../../../../commitizen"), // It is different for thi module itself and projects.
   config: {
     path: "cz-conventional-changelog",
   },
